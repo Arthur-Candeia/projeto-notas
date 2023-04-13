3a96fb081d9c656beedb4ceaac939c429cb7a91f
Arthur-Candeia <arthurheher@hotmail.com>
2023-04-12 23:43:25 -0300

    cores-das-notas

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,16 @@
 let notaEscrita = document.getElementById('novaNota')
+let corDaNota = document.getElementById('corNota')
 let main = document.getElementById('container')
 
+console.log(localStorage.corNote)
 if (localStorage.nota) {
   main.innerHTML = localStorage.nota;
+  let arrayCoresJson = localStorage.corNote
+  let arrayCores = JSON.parse(arrayCoresJson)
+  let corNotasExibidas = document.querySelectorAll('p')
+  for (let i = 0; i < arrayCores.length; i++) {
+    corNotasExibidas[i].style.backgroundColor = arrayCores[i];
+  }
 }
 
 function enviar() {
@@ -13,12 +21,37 @@ function enviar() {
     else{
       localStorage.nota = '<div>'+ '<pre>' + '<p>' + notaEscrita.value + '</p>' + '</pre>' + '</div>'
     }
-    main.innerHTML = localStorage.nota;
+    main.innerHTML = localStorage.nota
+
+    if (localStorage.corNote) {
+      let arrayCoresJson = localStorage.corNote //joga o localstorage pra dentro de um json
+      let arrayCores = JSON.parse(arrayCoresJson) //transforma o json em array
+      arrayCores.push(corDaNota.value) //push no array
+      let arrayCoresJsonAtualizado = JSON.stringify(arrayCores) //json atualizado
+      localStorage.corNote = arrayCoresJsonAtualizado
+    }
+    else {
+      let arrayCores = [corDaNota.value] //cria um array
+      let arrayCoresJson = JSON.stringify(arrayCores) //string JSON
+      localStorage.corNote = arrayCoresJson //salva no localstorage
+    }
+    let arrayCoresJson = localStorage.corNote
+    let arrayCores = JSON.parse(arrayCoresJson)
+    let ultimaNota = document.querySelectorAll('p')
+    for (let i = 0; i < arrayCores.length; i++) {
+      ultimaNota[i].style.backgroundColor = arrayCores[i];
+    }
   }
   notaEscrita.value = '';
 }
 
+function corteste() {
+  let teste = document.querySelector('textarea')
+  teste.style.backgroundColor = corDaNota.value
+}
+
 function agora() {
   localStorage.removeItem('nota')
+  localStorage.removeItem('corNote')
   location.reload()
 }
